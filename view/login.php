b0b8b4727fdbbe052961586bcc74f954db61c0d1
jhordinor@gmail.com <jhordinor@gmail.com>
2025-09-14 11:24:57 -0500

    #chore 02:  Login responsive

---
+++ view/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css">
+    <title>Login</title>
+</head>
+<body>
+    <img class="wave"  src="../img/wave.png" alt="">
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/bg.svg" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="../img/logo.png" alt="">
+                <h2>Farmacia Jhor</h2>
+                <div class="input-div dni">
+                    <div class="1">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>DNI</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="1">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                </div>
+                <a href="#">Create warpiece</a>
+                <input type="submit" class="btn" value="iniciar session">
+            </form> 
+        </div>
+    </div>
+</body>
+</html>
